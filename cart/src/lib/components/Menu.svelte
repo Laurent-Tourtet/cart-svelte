b1edbf8063e76935493e1ad1f2c58358455b596b
Laurent-Tourtet <laurent.tourtet@oclock.school>
2024-11-13 09:01:40 +0100

    ajout d'un menu en colonne latérale gauche

---
+++ cart/src/lib/components/Menu.svelte
@@ -0,0 +1,48 @@
+<main class="menu">
+    <section class="menu-nav">
+        <nav>
+            <ul>
+                <li><a href="/">Accueil</a></li>
+                <li><a href="/products">Produits</a></li>
+                <li>
+                    <select name="category" id="menu-category">
+                        <option value="all">Tous les produits</option>
+                        <option value="food">Alimentation</option>
+                        <option value="clothes">Vêtements</option>
+                        <option value="electronics">Électronique</option>
+                    </select>
+                </li>
+                <li><a href="/promo">Promos</a></li>
+            </ul>
+        </nav>
+    </section>
+</main>
+
+<style>
+    .menu {
+        width: 200px;
+        height: 100vh;
+        background-color: #f4f4f4;
+        padding: 20px;
+        box-sizing: border-box;
+    }
+
+    .menu-nav ul {
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+    }
+
+    .menu-nav li {
+        margin-bottom: 1rem;
+    }
+
+    .menu-nav a {
+        text-decoration: none;
+        color: #333;
+    }
+
+    #menu-category {
+        width: 100%;
+    }
+</style>
--- cart/src/routes/+layout.svelte
+++ cart/src/routes/+layout.svelte
@@ -1,15 +1,19 @@
 <script>
     import '../global.css';
-       
+    import Menu from '$lib/components/Menu.svelte';
 </script>
 
 <header class="top">
-
     <h1>Votre Boutique</h1>
-    
 </header>
- 
-<slot />
+
+<div class="layout-container">
+    <Menu />
+    <div class="content">
+        <slot />
+    </div>
+</div>
+
 <style>
     .top {
         background-color: #333;
@@ -17,7 +21,18 @@
         padding: 1rem;
         text-align: center;
     }
-    
 
-   
+    .layout-container {
+        display: flex;
+    }
+
+    /* Cibler la classe CSS `menu` du composant `Menu.svelte` */
+    .layout-container .menu {
+        flex-basis: 200px; /* Largeur fixe pour le menu */
+    }
+
+    .content {
+        flex: 1;
+        padding: 1rem;
+    }
 </style>
